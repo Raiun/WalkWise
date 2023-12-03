d4467c572c06b6a26905540f5635685d5ad37618
Raiun <ryan.man821@gmail.com>
2023-12-02 19:22:04 -0800

    more unlock.py implementation

--- unlock.py
+++ unlock.py
@@ -1,5 +1,6 @@
-import asyncio
 import os
+import asyncio
+import argparse
 from dotenv import load_dotenv
 from smartrent import async_login
 
@@ -7,16 +8,29 @@ load_dotenv()
 email = os.getenv("EMAIL")
 password = os.getenv("EMAIL_PASSWORD")
 
+parser = argparse.ArgumentParser(
+                    prog="Unlock.py",
+                    description="Unlock the door for recognized users")
+
+parser.add_argument("user", help="User passed from WalkWise neural network")
+
+args = parser.parse_args()
+print(args.user)
+
+recognized_users = ["Test", "Test2"]
+
 async def main():
     try:
         api = await async_login(email, password)
         locks = api.get_locks()
         first_lock = locks[0]
 
-        await first_lock.async_set_locked(False)
-        await asyncio.sleep(10)
-        await first_lock.async_set_locked(True)
+        if args.user in recognized_users:
+            await first_lock.async_set_locked(False)
+        else:
+            print("Unrecognized User! DOOR WILL LOCK!")
+            await first_lock.async_set_locked(True)
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
 
 asyncio.run(main())
