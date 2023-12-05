5e292f8cc2e5c420c7bbca6b9238e98fb98fafc3
Nate Webster <natewebster@Nates-MacBook-Air.local>
2023-12-05 15:34:59 -0800

    Updated manage_imu to call unlock.py

--- manage_imu.py
+++ manage_imu.py
@@ -2,11 +2,10 @@ import collect_imu_data as imu
 import model_inference as inference
 from model_inference import Gait1DCNN  # needed so Gait1DCNN symbol can be resolved
 import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
 import asyncio
 import os
 import numpy as np
+import unlock
 
 arduinoName = "Nate's Nano"
 newData = "new_data.txt"
@@ -16,16 +15,17 @@ async def main():
     await imu.collect_data(arduinoName,newData,collectionTime)
     
     model = torch.load("1D_CNN_model.pth")
-    # label_dict = {"hersh": 0, "nate": 1, "ryan": 2}
     name_dict = {0: "hersh", 1: "nate", 2: "ryan"}
     model.eval()  # Set the model to evaluation mode
-    
     sample = np.loadtxt(
             newData, delimiter=","
         ) 
     prediction = inference.predict(model, sample)
     predictedName = name_dict[prediction]
     print(predictedName)
+
+    os.system("python3 unlock.py " + predictedName) # call unlock.py
+
     os.remove(newData) # clear file for each iteration
 
 if __name__ == "__main__":
